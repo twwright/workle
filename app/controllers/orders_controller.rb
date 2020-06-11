8cab019fd7f23fc87f901d3857064afe3a931968
twwright <twwright@gmail.com>
2020-06-10 19:50:05 -0500

    Add new and create for orders model

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,18 @@
 class OrdersController < ApplicationController
   def index
   end
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(params.require(:order).permit(:date, :details))
+    @order.save
+    redirect_to @order
+  end
+
+  def show
+    @order = Order.find(params[:id])
+  end
 end
--- spec/features/order_spec.rb
+++ spec/features/order_spec.rb
@@ -13,7 +13,22 @@ describe 'navigate' do
       visit orders_path
       expect(page).to have_content(/Orders/) # Use RegEx to search for match of text
     end
-
-
   end
+
+  # Order creation
+  describe 'creation of an order' do
+    it 'has a form that can load successfully' do
+      visit new_order_path
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'can be created from new form page' do
+      visit new_order_path
+      fill_in 'order[date]', with: Date.today
+      fill_in 'order[details]', with: 'Details for order'
+      click_on 'Save'
+      expect(page).to have_content('Details for order')
+    end
+  end
+
 end
